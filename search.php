de3d6b726b472d0dcb1b40f3584607ae516876c0
Kaitlyn MacIntyre <kmac2017@u.northwestern.edu>
2015-11-22 06:11:10 -0600

    Changed output to JSON

--- search.php
+++ search.php
@@ -15,10 +15,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$gen = '%' . $_POST["general"] . '%';
-$book_name = '%' . $_POST["title"] . '%';
-$isbn = '%' . $_POST["isbn"] . '%';
-$class_name = '%' . $_POST["class"] . '%';
+$return = $_POST;
+
+$gen = '%' . $return["general"] . '%';
+$book_name = '%' . $return["title"] . '%';
+$isbn = '%' . $return["isbn"] . '%';
+$class_name = '%' . $return["class"] . '%';
 
 $sql = "SELECT * FROM Book_Information 
         WHERE (Book_name LIKE $book_name OR Book_name LIKE $gen) 
@@ -29,9 +31,20 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo $row["Book_name"] . "\n" . $row["Class_code"] . "\n" . 
+        $return["Book_name"] = $row["Book_name"];
+        $return["Class_code"] = $row["Class_code"]; 
+        $return["ISBN"] = $row["ISBN"];
+        $return["Seller_ID"] = $row["Seller_ID"];
+        $return["State"] = $row["State"]; 
+        $return["Price"] = $row["Price"];
+        $return["Location"] = $row["Location"]
+        $return["Negotiate"] = $row["Negotiate"];
+        $return["json"] = json_encode($return);
+        echo json_encode($return);
+        
+        /*echo $row["Book_name"] . "\n" . $row["Class_code"] . "\n" . 
         $row["ISBN"] . "\n" . $row["Seller_ID"] . "\n" . $row["State"] . "\n" . 
-        $row["Price"] . "\n" . $row["Location"] . "\n" . $row["Negotiate"] . "<br>";
+        $row["Price"] . "\n" . $row["Location"] . "\n" . $row["Negotiate"] . "<br>";*/
         /*echo "Seller ID: " . $row["Seller_ID"] . " - Class: " . $row["Class_code"] . " - Book: " . 
         $row["Book_name"] . " - ISBN: " . $row["ISBN"] . " - State: " . $row["State"] . " - Location: " . 
         $row["Location"] . " - Price: " . $row["Price"] . " - Negotiable: " . $row["Negotiate"] . "<br>";*/
